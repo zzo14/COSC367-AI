d31fb4bca880e8f8193bb03ea7dfb0091b27a189
zzo14 <71738569+zzo14@users.noreply.github.com>
2020-10-15 20:56:22 +1300

    Delete q2.py

--- superQuiz/sq2/q2.py
+++
@@ -1,7 +0,0 @@
-def depth(expression,x=0):
-    if not expression or not isinstance(expression,list):
-        return x
-    l = depth(expression[0],x + 1)
-    r = depth(expression[1:],x)
-
-    return max(l,r)
